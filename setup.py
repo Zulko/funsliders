af4c715a9584da0e33df8c3cb3df23fbf5ae041a
Zulko <valentin.zulkower@gmail.com>
2013-12-02 23:11:45 +0100

    I really really hate commits

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='funsliders',
-      version='0.0.0.1',
+      version='0.0.0.2',
       author='Zulko 2013',
-    description= "Funsliders are user-friendly Matplotlib sliders",
-    long_description=open('README.md').read(),
-    license=open('LICENSE.txt').read(),
-    keywords="pylab matplotlib slider interactive function",
-    packages= find_packages(exclude='docs'))
+      description= "Funsliders are user-friendly Matplotlib sliders",
+      long_description=open('README.md').read(),
+      license=open('LICENSE.txt').read(),
+      keywords="pylab matplotlib slider interactive function")
